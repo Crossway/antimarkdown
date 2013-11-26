902cb148312c5d441cd1622c13fe5c1b65faf29d
deyk <deyk@crossway.org>
2013-11-26 10:45:30 -0800

    Bumped to 0.2, added some metadata.

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 """setup.py -- setup file for antimarkdown
 """
+import os
 from setuptools import setup
+
+README = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst')
+
 
 setup(
     name = "antimarkdown",
@@ -9,15 +13,36 @@ setup(
     install_requires = [
         'lxml',
         'BeautifulSoup',
-        ],
+    ],
 
     package_data = {
         '': ['*.txt', '*.html'],
-        },
+    },
     zip_safe = False,
 
-    version = "0.1.1",
+    version = "0.2",
     description = "HTML to Markdown converter.",
+    long_description = open(README).read(),
     author = "David Eyk",
     author_email = "deyk@crossway.org",
-    )
+    url = "http://github.com/Crossway/antimarkdown/",
+    license = 'BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Communications :: Email :: Filters',
+    ],
+)
